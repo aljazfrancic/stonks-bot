1b059326bb58952ddfd4aec9a1383d694c66204f
aljazfrancic <aljaz.francic@gmail.com>
2021-10-20 19:43:42 +0200

    hurricane name fix (api change)

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ async def print_graph(coins, data, timestamps, longreadable, longstamp, colors, 
     plt.close()
 
 async def call_print_graph(discord_channel, days):
-    coins = ["bitcoin", "ethereum", "monero", "chainlink", "sherpa", "snowball-token", "avalaunch", "yield-yak", "baguette", "pangolin", "yay-games", "elk-finance", "lydia-finance", "penguin-finance", "gondola-finance", "benqi", "kalao", "avme", "spore", "hurricane-token", "avalanche-2"] 
+    coins = ["bitcoin", "ethereum", "monero", "chainlink", "sherpa", "snowball-token", "avalaunch", "yield-yak", "baguette", "pangolin", "yay-games", "elk-finance", "lydia-finance", "penguin-finance", "gondola-finance", "benqi", "kalao", "avme", "spore", "hurricaneswap-token", "avalanche-2"] 
     colors = ["ffffff", "9e9e9e", "ff8800", "009dff", "00ff08", "00ddff", "ffff00", "277d3e", "fffa96", "fc9505", "78aeff", "785211", "ff00c8", "006157", "808080", "e6d439", "678569", "ffbdfb", "c5a6ff", "ad6ff7", "ff0000"]
     linestyle = [":", ":", ":", ":", "-", "-", "-", "-", "-", "-", "-.", "-.", "-.", "-.", "-.", "-.", "-.", "-.", "-.", "-", "-"]
     data = []
